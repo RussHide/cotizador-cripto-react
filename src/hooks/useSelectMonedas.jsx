235697cd7e8c13688086ed915f47605721cc3cae
Fabian Hide <khaledidfc@gmail.com>
2022-01-14 19:35:40 -0600

    Styled Components al Select

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -2,7 +2,19 @@ import styled from "@emotion/styled";
 
 const Label = styled.label`
     color: white;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
 `
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
+`
+
 
 export const useSelectMonedas = (label, opciones) => {
     const SelectMonedas = () => (
@@ -10,7 +22,7 @@ export const useSelectMonedas = (label, opciones) => {
             <Label htmlFor="">
                 {label}
             </Label>
-            <select name="" id="">
+            <Select name="" id="">
                 <option value="">Seleccione</option>
                 {opciones.map(opcion => (
                     <option
@@ -18,7 +30,7 @@ export const useSelectMonedas = (label, opciones) => {
                         value={opcion.id}
                     >{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </>
     )
     return [SelectMonedas]
